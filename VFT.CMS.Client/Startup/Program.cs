243806619ffcd6d2ecf5742cb6a096e708ff0db9
Wreckecr <110579816+Wreckecr@users.noreply.github.com>
2023-06-28 11:00:10 +0700

    Add references to projects

---
+++ VFT.CMS.Client/Startup/Program.cs
@@ -0,0 +1,17 @@
+namespace VFT.CMS.Client.Startup
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] agrs) =>
+            Host.CreateDefaultBuilder(agrs)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
