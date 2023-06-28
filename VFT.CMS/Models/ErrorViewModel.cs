91f5c0c8c42f3211902e8aa925f974247b2eb52e
Wreckecr <110579816+Wreckecr@users.noreply.github.com>
2023-06-28 10:10:25 +0700

    Add project files.

---
+++ VFT.CMS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VFT.CMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
