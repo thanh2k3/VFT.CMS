80a8da27de912cf284c85de67d0d57a4ab8e87a8
Wreckecr <110579816+Wreckecr@users.noreply.github.com>
2023-11-01 13:14:41 +0700

    Update Product 3rd - edit font - Admin

--- VFT.CMS.Application/Products/ProductService.cs
+++ VFT.CMS.Application/Products/ProductService.cs
@@ -14,94 +14,118 @@ using VFT.CMS.Repository.Data;
 
 namespace VFT.CMS.Application.Products
 {
-	public class ProductService : IProductService
-	{
-		private readonly AppDBContext _context;
-		private readonly IMapper _mapper;
-		private readonly IHostingEnvironment _environment;
+    public class ProductService : IProductService
+    {
+        private readonly AppDBContext _context;
+        private readonly IMapper _mapper;
+        private readonly IHostingEnvironment _environment;
 
-		public ProductService(AppDBContext context, IMapper mapper, IHostingEnvironment environment)
-		{
-			_context = context;
-			_mapper = mapper;
-			_environment = environment;
-		}
+        public ProductService(AppDBContext context, IMapper mapper, IHostingEnvironment environment)
+        {
+            _context = context;
+            _mapper = mapper;
+            _environment = environment;
+        }
 
-		public async Task<IEnumerable<ProductDto>> GetAll()
-		{
-			var products = await _context.Products.Include(x => x.Category).ToListAsync();
-			var productsDto = _mapper.Map<IEnumerable<ProductDto>>(products);
+        public async Task<IEnumerable<ProductDto>> GetAll()
+        {
+            var products = await _context.Products.Include(x => x.Category).ToListAsync();
+            var productsDto = _mapper.Map<IEnumerable<ProductDto>>(products);
 
-			return productsDto;
-		}
+            return productsDto;
+        }
 
-		public async Task<ProductDto> GetById(int id)
-		{
-			var product = await _context.Products.Include(x => x.Category)
-												 .FirstOrDefaultAsync(x => x.Id == id);
-			var productDto = _mapper.Map<ProductDto>(product);
+        public async Task<ProductDto> GetById(int id)
+        {
+            var product = await _context.Products.Include(x => x.Category)
+                                                 .FirstOrDefaultAsync(x => x.Id == id);
+            var productDto = _mapper.Map<ProductDto>(product);
 
-			return productDto;
-		}
+            return productDto;
+        }
 
-		public async Task<bool> Create(CreateProductDto model, IFormFile? image)
-		{
-			var product = _mapper.Map<Product>(model);
-			var searchProduct = await _context.Products.FirstOrDefaultAsync(x => x.Name == product.Name);
-			if (searchProduct == null)
-			{
-				if (image != null)
-				{
-					var imageName = "product_" + Guid.NewGuid().ToString() + "_" + image.FileName;
+        public async Task<bool> Create(CreateProductDto model, IFormFile? image)
+        {
+            var product = _mapper.Map<Product>(model);
+            var searchProduct = await _context.Products.FirstOrDefaultAsync(x => x.Name == product.Name);
+            if (searchProduct == null)
+            {
+                if (image != null)
+                {
+                    var imageName = "product_" + Guid.NewGuid().ToString() + "_" + image.FileName;
 
-					var name = Path.Combine(_environment.WebRootPath + "/image/product", imageName);
-					await image.CopyToAsync(new FileStream(name, FileMode.Create));
-					product.Image = "image/product/" + imageName;
-				} else
-				{
-					product.Image = "image/product/noimage.PNG";
-				}
+                    var name = Path.Combine(_environment.WebRootPath + "/image/product", imageName);
+                    await image.CopyToAsync(new FileStream(name, FileMode.Create));
+                    product.Image = "image/product/" + imageName;
+                }
+                else
+                {
+                    product.Image = "image/product/noimage.PNG";
+                }
 
-				await _context.Products.AddAsync(product);
-				await Save();
+                await _context.Products.AddAsync(product);
+                await Save();
 
-				return true;
-			}
+                return true;
+            }
 
-			return false;
-		}
+            return false;
+        }
 
-		public async Task Update(EditProductDto model, IFormFile? image)
-		{
-			var product = _mapper.Map<Product>(model);
+        public async Task Update(EditProductDto model, IFormFile? image)
+        {
+            //var product = _mapper.Map<Product>(model);
+            //var data = await _context.Products.FirstOrDefaultAsync(x => x.Id == product.Id);
 
-			if (image != null)
-			{
-				var imageName = "product_" + Guid.NewGuid().ToString() + "_" + image.FileName;
+            //if (data != null)
+            //{
+            //    if (image != null)
+            //    {
+            //        var imageName = "product_" + Guid.NewGuid().ToString() + "_" + image.FileName;
 
-				var name = Path.Combine(_environment.WebRootPath + "/image/product", imageName);
-				await image.CopyToAsync(new FileStream(name, FileMode.Create));
-				product.Image = "image/product/" + imageName;
-			}
-			else
-			{
-				product.Image = "image/product/noimage.PNG";
-			}
+            //        var name = Path.Combine(_environment.WebRootPath + "/image/product", imageName);
+            //        await image.CopyToAsync(new FileStream(name, FileMode.Create));
+            //        product.Image = "image/product/" + imageName;
+            //    }
+            //    else
+            //    {
+            //        //product.Image = "image/user/noimage.png";
+            //        product.Image = data.Image;
+            //    }
 
-			_context.Products.Update(product);
-			await Save();
-		}
+            //    _context.Products.Update(product);
+            //    await Save();
+            //}
 
-		public async Task Delete(int id)
-		{
-			var product = await _context.Products.FirstOrDefaultAsync(x => x.Id == id);
-			_context.Products.Remove(product);
-			await Save();
-		}
+            var product = _mapper.Map<Product>(model);
 
-		public async Task Save()
-		{
-			await _context.SaveChangesAsync();
-		}
-	}
+            if (image != null)
+            {
+                var imageName = "product_" + Guid.NewGuid().ToString() + "_" + image.FileName;
+
+                var name = Path.Combine(_environment.WebRootPath + "/image/product", imageName);
+                await image.CopyToAsync(new FileStream(name, FileMode.Create));
+                product.Image = "image/product/" + imageName;
+            }
+            else
+            {
+                product.Image = "image/user/noimage.png";
+            }
+
+            _context.Products.Update(product);
+            await Save();
+        }
+
+        public async Task Delete(int id)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(x => x.Id == id);
+            _context.Products.Remove(product);
+            await Save();
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
 }
